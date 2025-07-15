d0f76ef03478a18fc129ad39d2e21dd62b4b8a36
praveen-9014 <151435506+praveen-9014@users.noreply.github.com>
2025-07-15 06:25:27 +0530

    Day-9 task is completed

---
+++ Day-9 react-task/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../index.css"
+
+const Footer = () => {
+  return (
+    <footer>
+      &copy; 2025 EMS. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
