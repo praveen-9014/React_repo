c085a0e68e350c35f4d5023e07c00740c45cf888
praveen-9014 <151435506+praveen-9014@users.noreply.github.com>
2025-07-16 23:00:19 +0530

    Day-12 work is completed

---
+++ Day-12/src/components/Login.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Login = ({ onLogin }) => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin(event) {
+    event.preventDefault();
+    try {
+      const token = await axios.post("http://localhost:3001/api/auth/login", {
+        userName,
+        password,
+      });
+
+      localStorage.setItem("token", token.data.token);
+      alert("Login Successful");
+      setUserName("");
+      setPassword("");
+      onLogin();
+    } catch (e) {
+      console.error("Login Error", e);
+      alert("Invalid Credentials");
+    }
+  }
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6 col-lg-5">
+          <div className="card shadow-sm bg-light text-dark">
+            <div className="card-body">
+              <h3 className="text-center mb-4">Login</h3>
+              <form onSubmit={handleLogin}>
+                <div className="mb-3">
+                  <label htmlFor="userName" className="form-label">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control border-dark"
+                    id="userName"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control border-dark"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+
+                <div className="d-grid mt-4">
+                  <button type="submit" className="btn btn-primary">
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
